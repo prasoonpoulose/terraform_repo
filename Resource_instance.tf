333f4a028848dad2d7b1815d3c8a3947386c3a71
prasoonpoulose <prasoonpoulose@gmail.com>
2025-07-19 11:24:15 +0100

    First Commit

---
+++ Resource_instance.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+}
+
+resource "aws_instance" "linux" {
+  count = "1"
+  ami = var.ec2_ami
+  instance_type = var.instance_type
+  associate_public_ip_address = true
+  key_name= "linux_ec2"
+  tags = {
+    Name = "Ansible"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo amazon-linux-extras install ansible2
+    ansible --version
+  EOF
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    default = "eu-west-2"
+}
+
+variable "ec2_ami"{
+  default = "ami-02f617729751b375a"
+}
+
+variable "instance_type"{
+  default = "t2.micro"
+}
